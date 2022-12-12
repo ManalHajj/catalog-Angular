0d412d9caf11f694ea6550d895f93f717642cf05
Manal Hajjami <81756572+ManalHajj@users.noreply.github.com>
2022-12-12 21:18:08 +0100

    Add files via upload

---
+++ inventory-service/inventory-service/src/main/java/org/sid/inventoryservice/security/KeycloakAdapterConfig.java
@@ -0,0 +1,18 @@
+package org.sid.inventoryservice.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakAdapterConfig {
+    @Bean
+    //pour pas utiliser un fichier json et direct utiliser la config dans app properties
+    public KeycloakSpringBootConfigResolver springBootConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
+    }
+    /*@Bean
+    KeycloakRestTemplate keycloakRestTemplate (KeycloakClientRequestFactory keycloakClientRequestFactory){
+        return new KeycloakRestTemplate(keycloakClientRequestFactory);
+    }*/
+}
---
+++ inventory-service/inventory-service/src/main/java/org/sid/inventoryservice/security/KeycloakSecurityConfig.java
@@ -0,0 +1,35 @@
+package org.sid.inventoryservice.security;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+
+@KeycloakConfiguration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider()); // c pas à moi de gérer les utilisateurs et les roles on va le déleguer à un provider keycloak
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll(); // pour accéder à /products ou /suppliers l'utilisateur doit étre authentifié
+        http.headers().frameOptions().disable();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
